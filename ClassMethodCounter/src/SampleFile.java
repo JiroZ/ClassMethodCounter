f29b22a9f8f62d7dd8af843ff948b3c296370337
Bhavishya <bhavishya20@gmail.com>
2019-06-21 17:35:58 +0530

    [Final update] refined code and made sure its understandable for everyone

--- ClassMethodCounter/src/SampleFile.java
+++ ClassMethodCounter/src/SampleFile.java
@@ -5,23 +5,23 @@
 
 public class SampleFile {
 
-    public void function1(String a){
-
+    public void function1(String[] Args){
+        System.out.println("main");
     }
 
     protected void function2(String a) {
-
+        System.out.println("main");
     }
 
     private void function3(String a) {
-
+        System.out.println("main");
     }
 
     public void function4() {
-
+        System.out.println("main");
     }
 
     public void function5(){
-
+        System.out.println("main");
     }
 }
